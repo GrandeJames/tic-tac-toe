3074b31ba8bd1c25a62ed60e5ff35e05776ec4b6
James Grande <james.grande6@gmail.com>
2021-07-23 09:47:25 -1000

    Add game-board module and player factory

---
+++ script.js
@@ -0,0 +1,60 @@
+// Create the gameBoard module
+/*
+    Add a gameBoard array which consists of the Xs and Os there should be 9
+
+    Create the grid for the gameboard 3x3
+        Get the gameboard element
+        Set the template rows style (3 with each a size of 1fr)
+        Set the template columns style (3 with each a size of 1fr)
+    
+        Then use for loop for the total amount of cells
+            Create a div and set its class to cell
+            Add that div to the gameboard
+            Style the cell in css
+
+    Add the click listener for each cell
+        when clicked it should set the textContent to either x or o
+        at the index add the letter to the index in the array
+*/
+
+const gameBoard = (() => {
+  let board = [];
+
+  const BOARD_SIZE = 9;
+  for (let i = 0; i < BOARD_SIZE; i++) {
+    board.push("");
+  }
+
+  const getCellSign = index => board[index];
+  const setCellSign = (index, sign) => (board[index] = sign);
+  const reset = () => board.forEach((cell, index) => (board[index] = ""));
+
+  return { getCellSign, setCellSign, reset };
+})();
+
+const playerFactory = () => {
+  let playerSign;
+
+  const getSign = () => playerSign;
+  const setSign = sign => (playerSign = sign);
+
+  return { getSign, setSign };
+};
+
+const player1 = playerFactory();
+const player2 = playerFactory();
+
+console.log(player1.getSign());
+console.log(player2.getSign());
+
+player1.setSign("X");
+player2.setSign("O");
+
+console.log("player1 sign is " + player1.getSign());
+console.log("player2 sign is " + player2.getSign());
+
+console.log(gameBoard.getCellSign(3));
+gameBoard.setCellSign(3, "X");
+console.log(gameBoard.getCellSign(3));
+gameBoard.reset();
+console.log(gameBoard.getCellSign(3));
