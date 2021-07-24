7959d447b6f4f473f7d3c0b9ed37331d0b5939b8
James Grande <james.grande6@gmail.com>
2021-07-24 09:52:47 -1000

    Refactor code

--- script.js
+++ script.js
@@ -1,3 +1,117 @@
+const gameController = (() => {
+  const _player1 = player("X");
+  const _player2 = player("O");
+
+  let _player1Choosing = true;
+
+  const getPlayer1 = () => _player1;
+  const getPlayer2 = () => _player2;
+
+  const toggleNextPlayer = cell => {
+    if (_player1Choosing) {
+      displayController.setMark(cell, _player1.getSign());
+    } else {
+      displayController.setMark(cell, _player2.getSign());
+    }
+    _player1Choosing = !_player1Choosing;
+  };
+
+  const announceGameResult = () => {
+    if (gameBoard.getBoard().every(element => element)) {
+      displayController.setMessage("It's a tie!");
+      return;
+    }
+
+    let winCombos = gameBoard.getWinCombos();
+
+    for (let i = 0; i < winCombos.length; i++) {
+      if (_isWinner(winCombos[i], _player1.getSign())) {
+        displayController.setMessage("Player 1 won!");
+        break;
+      } else if (_isWinner(winCombos[i], _player2.getSign())) {
+        displayController.setMessage("Player 2 won!");
+        break;
+      }
+    }
+  };
+
+  const _isWinner = (arr, sign) => {
+    return arr.every(innerArrVal => gameBoard.getBoard()[innerArrVal] === sign);
+  };
+
+  const reset = () => {
+    // RESET THE CELLS: REMOVE ITS TEXT CONTENT
+    // REMOVE MESSAGE IF THERE IS
+    // RESET THE NEXT PLAYER
+  };
+
+  return {
+    getPlayer1,
+    getPlayer2,
+    announceGameResult,
+    toggleNextPlayer,
+  };
+})();
+
+function player(sign) {
+  const getSign = () => sign;
+
+  return { getSign };
+}
+
+const displayController = (() => {
+  _createGameBoard();
+  _addCellClickListener();
+
+  function _createGameBoard() {
+    const gameBoardElement = document.querySelector(".game-board");
+
+    const GRID_SIZE = 3;
+
+    createGrid(GRID_SIZE);
+    addCellsToGrid(GRID_SIZE);
+
+    function createGrid(size) {
+      gameBoardElement.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+      gameBoardElement.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    }
+
+    function addCellsToGrid(size) {
+      for (let i = 0; i < size * size; i++) {
+        const cellDiv = document.createElement("div");
+        cellDiv.className = "cell";
+        cellDiv.setAttribute("data-index", i);
+
+        gameBoardElement.appendChild(cellDiv);
+      }
+    }
+  }
+
+  function _addCellClickListener() {
+    const cells = document.querySelectorAll(".cell");
+
+    cells.forEach(cell =>
+      cell.addEventListener("click", () => {
+        if (!cell.textContent) {
+          gameController.toggleNextPlayer(cell);
+          gameController.announceGameResult();
+        }
+      })
+    );
+  }
+
+  const setMark = (cell, sign) => {
+    cell.textContent = sign;
+    gameBoard.setCellSign(cell.getAttribute("data-index"), sign);
+  };
+
+  const setMessage = text => {
+    document.querySelector(".message").textContent = text;
+  };
+
+  return { setMessage, setMark };
+})();
+
 const gameBoard = (() => {
   let _board = [];
 
@@ -28,110 +142,6 @@ const gameBoard = (() => {
   ];
 
   return { getCellSign, setCellSign, reset, getBoard, getWinCombos };
-})();
-
-const Player = sign => {
-  const getSign = () => sign;
-
-  return { getSign };
-};
-
-const displayController = (() => {
-  const _createGameBoard = () => {
-    const gameBoardElement = document.querySelector(".game-board");
-
-    const GRID_SIZE = 3;
-    gameBoardElement.style.gridTemplateRows = `repeat(${GRID_SIZE}, 1fr)`;
-    gameBoardElement.style.gridTemplateColumns = `repeat(${GRID_SIZE}, 1fr)`;
-
-    for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
-      const cellDiv = document.createElement("div");
-      cellDiv.className = "cell";
-      cellDiv.setAttribute("data-index", i);
-
-      gameBoardElement.appendChild(cellDiv);
-    }
-  };
-
-  const setMark = (cell, sign) => {
-    cell.textContent = sign;
-    gameBoard.setCellSign(cell.getAttribute("data-index"), sign);
-  };
-
-  const setMessage = text => {
-    const message = document.querySelector(".message");
-    message.textContent = text;
-  };
-
-  const _addCellClickListener = () => {
-    const cells = document.querySelectorAll(".cell");
-    let isGameOver = false;
-
-    cells.forEach(cell =>
-      cell.addEventListener("click", () => {
-        if (!cell.textContent) {
-          gameController.toggleNextPlayer(cell);
-          gameController.getWinner();
-        }
-      })
-    );
-  };
-
-  const reset = () => {
-    // RESET THE CELLS: REMOVE ITS TEXT CONTENT
-    // REMOVE MESSAGE IF THERE IS
-    // RESET THE NEXT PLAYER
-  };
-
-  _createGameBoard();
-  _addCellClickListener();
-
-  return { setMessage, setMark };
-})();
-
-const gameController = (() => {
-  displayController;
-
-  const _player1 = Player("X");
-  const _player2 = Player("O");
-
-  const getPlayer1 = () => _player1;
-  const getPlayer2 = () => _player2;
-
-  const getWinner = () => {
-    if (gameBoard.getBoard().every(element => element)) {
-      displayController.setMessage("It's a tie!");
-      return;
-    }
-
-    let winCombos = gameBoard.getWinCombos();
-    for (let i = 0; i < winCombos.length; i++) {
-      if (_isWinner(winCombos[i], _player1.getSign())) {
-        displayController.setMessage("Player 1 won!");
-        break;
-      } else if (_isWinner(winCombos[i], _player2.getSign())) {
-        displayController.setMessage("Player 2 won!");
-        break;
-      }
-    }
-  };
-
-  const _isWinner = (arr, sign) => {
-    return arr.every(innerArrVal => gameBoard.getBoard()[innerArrVal] === sign);
-  };
-
-  let _player1Choosing = true;
-
-  const toggleNextPlayer = cell => {
-    if (_player1Choosing) {
-      displayController.setMark(cell, _player1.getSign());
-    } else {
-      displayController.setMark(cell, _player2.getSign());
-    }
-    _player1Choosing = !_player1Choosing;
-  };
-
-  return { getPlayer1, getPlayer2, getWinner, toggleNextPlayer };
 })();
 
 gameController;
